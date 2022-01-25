83dbafda5e9c48b8df7ffe4b09d8d1e24f32ca39
kiranbaby14 <kiranbaby256@gmail.com>
2022-01-25 21:29:38 +0530

    todo list

--- src/components/addItem.js
+++ src/components/addItem.js
@@ -8,7 +8,8 @@ let AddItem = (props) =>{
 
     //Add the newly entered value onto the items list
     let addTodo = () => {
-
+        // Clear the input box upon pressing Enter
+        document.getElementById('inpBtn').value = ''
         let newObj = {
                         name: val,
                         crossed: false
@@ -20,13 +21,16 @@ let AddItem = (props) =>{
 
     return <>
             <input type="text" 
+                   id='inpBtn'
                    className="form-control add-task" 
                    placeholder="New Task..." 
                    onChange={ e => { setVal(e.target.value) }}
                     //once Enter key is pressed call addTodo function
                    onKeyUp={ event => {
                        if(event.key === 'Enter'){
+                
                            addTodo();
+                           
                        }
                     }}/>
     </>
